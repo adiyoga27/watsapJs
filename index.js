b84eaeb6441d43e1e9cad6aad0c50634e19a5d3a
adiyoga <adiyoga27new@gmail.com>
2023-05-19 10:12:39 +0800

    permission

--- index.js
+++ index.js
@@ -78,14 +78,14 @@ const createSession = function(id, description) {
     puppeteer: {
       headless: true,
       args: [
-        // '--no-sandbox',
-        // '--disable-setuid-sandbox',
-        // '--disable-dev-shm-usage',
-        // '--disable-accelerated-2d-canvas',
-        // '--no-first-run',
-        // '--no-zygote',
-        // '--single-process', // <- this one doesn't works in Windows
-        // '--disable-gpu'
+        '--no-sandbox',
+        '--disable-setuid-sandbox',
+        '--disable-dev-shm-usage',
+        '--disable-accelerated-2d-canvas',
+        '--no-first-run',
+        '--no-zygote',
+        '--single-process', // <- this one doesn't works in Windows
+        '--disable-gpu'
       ],
     },
     authStrategy: new LocalAuth({
